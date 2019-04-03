fe4a88bd941b4b0b07ed9d5d8bce5f0d165ef98d
pen@seeapp.com <pen@seeapp.com>
2019-04-03 20:26:50 +0800

    about des

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="about">
-    <div><img src="https://tnslog.com.my/wp-content/uploads/2016/05/homeslider_1.jpg" alt=""></div>
-    <p>深圳市特恩斯货运代理有限公司（TNS INTERNATIONAL FREIGHT AGENCY CO.,LTD OF SHENZHNE ）于20017年9经深圳市工商行政管理局注册成立的公司，
-      总公司在马来西亚 ，注册资本500万元, 年轻、专业与效率是公司营运的特色。
-      <p></p>       
-      我公司目前的经营范围包括：海运、陆运、空运、货物报关、进出口贸易,仓储等业务。公司机构包括财务、行政、业务（国内、国外）、操作部(海运、陆运、空运)、客服、车队 。 
+  <div >
+    <div class="about-img"></div>
+    <div class="container about-des">
+    <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;深圳市特恩斯货运代理有限公司（TNS INTERNATIONAL FREIGHT AGENCY CO.,LTD OF SHENZHNE ）于20017年9经深圳市工商行政管理局注册成立的公司，
+      总公司在马来西亚 ，注册资本500万元, 年轻、专业与效率是公司营运的特色。</p>
+      <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;我公司目前的经营范围包括：海运、陆运、空运、货物报关、进出口贸易,仓储等业务。公司机构包括财务、行政、业务（国内、国外）、操作部(海运、陆运、空运)、客服、车队 。 
       不断完善专业的服务和标准的作业流程，并将继续秉持“为客户全心全意服务”的经营理念，提供最佳的运输服务 来符合国内外顾客的需求和期望，并且我们与业界多家大型物流公司、船务公司建立了战略同盟关系，无论何时何地，
-      联系我们，能够在第一时间给您提供第一质量的服务。 </p>
+      联系我们，能够在第一时间给您提供第一质量的服务。</p>
+    </div>
+
   </div>
 </template>
+<style lang="scss" scoped>
+  .about-img {
+    height: 300px;
+    background-image: url(https://tnslog.com.my/wp-content/uploads/2016/05/homeslider_1.jpg);
+    background-size: cover;
+  }
+  .about-des {
+    padding-top: 50px;
+    text-align: left;
+  }
+</style>
